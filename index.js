056b9385df53a51caf09cf4669e7e80341320aa0
komalrani24 <komal.rani@softradix.in>
2023-09-05 18:41:43 +0530

    sum and multiply

---
+++ index.js
@@ -0,0 +1,43 @@
+//wap to print table of given number
+// var num;
+// num = 3;
+// for (let i = 1; i <=10; i++) {
+//   t = num * i;
+
+//   console.log(t)
+// }
+//function
+// function cal(a, b, type) {
+//   if (type == "sum") {
+//     let sum = a + b;
+//     return sum;
+
+//   } else if (type == "minus") {
+//    let minus = a - b;
+//    return minus;
+
+//   }
+// }
+
+//
+// function cal(a, b, type, name) {
+//   if (type == "mul") {
+//     var mul = a * b;
+//     return mul + " " + name;
+//   } else if (type == "div") {
+//     var div = a / b;
+//     return div + " " + name;
+//   }
+// }
+// var result = cal(8, 3, "mul", "komal");
+// console.log(result);
+// var result1 = cal(90, 10, "div", "neha");
+// console.log(result1);
+function age(a) {
+  if (a >= 18) {
+    console.log("you are eligible for voting");
+  } else {
+    console.log("not eligible for voting");
+  }
+}
+var result = age(18);
